b77a5f7bb56cbad4e440b6b953285065f13eaa9f
michael <johnvianney33@gmail.com>
2020-12-02 14:31:36 +0100

    Add RTC
    Modify methods/functions
    
    Read values as string from the RTC
    
    set function to keep a watch on power on the Arduino pins

--- MOLCOM.ino
+++ MOLCOM.ino
@@ -10,11 +10,17 @@
  * 8. Configure key 'B' when pressed will display a screen where the information as regards hour of power can be retrived, 
  *    It should be in such a way that when you fill the day, it will pop up the next day you want and perform the calculation of hours.
 */
-
+#include <RTClib.h>
+#include <Wire.h>
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 #include <EEPROM.h>
 #include <TinyGPS.h>
+
+
+//initializing the RTC 
+RTC_DS1307 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 //Setting the parameter of the geolocation sensor
 // Rx and Tx of GPS Sensor to the 18(Tx) and 19(Rx) of the Arduino
@@ -23,9 +29,13 @@ TinyGPS gps;
 // setting the parameters for the keypad
 const int Size = 6;
 char special_keys[Size] = {'A','B','C','D','*','#'};
+String year_h, month_h, day_h, holder_h;
+String year_i, month_i, day_i, holder_i;
 char meter_no[11];
-byte value;
+byte valu;
 char holder[11];
+int count = 0; // Holds the count time for power duration.
+int count_holder = 0; // This variable holds the value of the count at any time there is loss of power, so that a memory continual recounting can be done.
 char meter_no_confirm[11];
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
@@ -45,11 +55,28 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 // Since I will be writing to the LCD the RW pin need not be grounded.
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);  // RS,EN,D4,D5,D6,D7
 
+
+// Setting the pinmode for the Red, Yellow and Blue phases. These three pins are Inputs
+
+int Yphase_pin = A8;
+int Rphase_pin = A9;
+int Bphase_pin = A10;
+int buzzer_pin = 5;// The buzzer pin is the output pin
+
+char dateday_holder; // Variable for holding the day and the date for a particular day
+
 void setup() {
+  Serial1.begin(9600);
+  pinMode(Yphase_pin, INPUT);
+  pinMode(Rphase_pin, INPUT);
+  pinMode(Bphase_pin, INPUT);
+  pinMode(buzzer_pin, OUTPUT);
+  
   // set up the LCD's number of columns and rows:
   //initial_eeprom_clearing();
   lcd.begin(20, 4);
-  Serial1.begin(9600);
+  rtc.begin();
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   // Print a welcome message to the screen, displaying the company name and the name of the project
   lcd.print("MOLCOM MULTICONCEPTS");
   lcd.setCursor(2,1);
@@ -58,7 +85,9 @@ void setup() {
   lcd.print("DURATION");
   delay(3000);
   lcd.clear();
+  check_current_date_per_day();
   check_meter_no_eeprom();
+  // Immediately the system starts get the time and store in a variable
   delay(1000);
   loop();
 }
@@ -70,31 +99,46 @@ void loop() {
     if(gps.encode(Serial1.read()))// encode gps data
     { 
     gps.f_get_position(&lat,&lon); // get latitude and longitude
-
+    
+    //Latitude
     lcd.setCursor(0,2);
     lcd.print("Latitude:");
     lcd.setCursor(10,2);
     lcd.print(lat,6);
-   
     
     //Longitude
-      lcd.setCursor(0,3);
-      lcd.print("Longitude:");
-      lcd.setCursor(11,3);
-      lcd.print(lon,6);
-
-      
-     
+    lcd.setCursor(0,3);
+    lcd.print("Longitude:");
+    lcd.setCursor(11,3);
+    lcd.print(lon,6);
    }
   }
+  /* SEQUENCE OF OPERATION FROM THIS POINT
+    * If a high is detected in any of the three(3) phases then using a OR logic, activate the buzzer for 1000ms and deactivate
+    * create a variable that will hold the date, and day of the week
+    * create a variable that will hold the counts to mark the periods there was power from any of the 3phases
+    * In the loop process each time the variable holding the day confirms with the RTC reading that the day or date is not thesame, 
+    * take the value of count stored in the predefined variable and with that date push it to the EEPROM, Memory_Card,to save it.
+    * After that set back the counter variable to integer 0 and the date or day variable to an empty string.
+    * Send the data in the EEPROM immediately to the server via the GSM/GPRS module.
+   */
+   RYB_Phase_checker();
  
+}
+void check_current_date_per_day(){
+  DateTime now = rtc.now();
+  year_i= now.year();
+  month_i = now.month();
+  day_i = now.day();
+  holder_i = year_i.concat(month_i.concat(day_i));
+  
 }
 
 void check_meter_no_eeprom(){
     // Check for meter_no in EEPROM. If meter_no exist display it to the screen, else print no meter_no, call set_meter_no()
     for(int address = 0; address < 11; address++){
-     value = EEPROM.read(address);
-     holder[address] = value;
+     valu = EEPROM.read(address);
+     holder[address] = valu;
     }
     if(holder[0]!=0){
       lcd.setCursor(0,0);
@@ -105,8 +149,6 @@ void check_meter_no_eeprom(){
       lcd.print("Bearing Location");
        
       }
-      
-      
      else{
       lcd.setCursor(1,0);
       lcd.print("NO METER NUMBER");
@@ -116,8 +158,6 @@ void check_meter_no_eeprom(){
       set_meter_no();
       }
     }
-
-
 
  void warning_signal(){ 
       // Initial warning that the meter no has to be a valid number and not a character.
@@ -138,10 +178,6 @@ void check_meter_no_eeprom(){
   lcd.print("ENTER METER_NO:");
   enter_num();
   }
-  
- 
-
-
 
 void enter_num(){
     // Call entry method that takes in the numbers
@@ -167,7 +203,6 @@ void enter_num(){
        }
     }
  
-
 void entry(){
     // Within the context of inputting the meter_no if A,B,C,D,*,# is pressed, display invalid character. 
     // But if after the entering the value if A is pressed it means enter
@@ -265,8 +300,49 @@ void entry(){
     
     }
 
- 
+  void RYB_Phase_checker(){
+    // This method checks the 3 phases for power 
+    
+    if ((digitalRead(Yphase_pin) == HIGH) || (digitalRead(Bphase_pin) == HIGH) || (digitalRead(Rphase_pin) == HIGH)){
+      count_holder = count++;
+      activate_rtc(); // i have to store this value once.
+      if(holder_h != holder_i){
+        /* Once there is power and the date stored is not equal, that means we have entered a new day
+         * make the count = 0, to restart for the next day,
+         * push the date and count_holder value into the EEPROM, Mscard, and send using the GSM module
+         * we have the get the new date to use as a checking parameter, against the looping date
+         * reinitialize count_holder = 0, for a new day.
+        */
+       count = 0;
+       
+        }
+      } 
 
+     else if ((digitalRead(Yphase_pin) == LOW) || (digitalRead(Bphase_pin) == LOW) || (digitalRead(Rphase_pin) == LOW)){
+      /* Check if the we are still in the same day
+       *  if we are still on the same day, take the value of the count_holder store and maintain it.
+       *  if we have entered another day reintialize the count_holder to be equal to 0, likewise the count
+       *  store the count_holder and the day to the EEPROM and MScard and likewise send the information
+      */
+      }
+    
+    }
+
+ void activate_rtc(){
+  // Get the day when the 3phases pin were high
+  // create a variable to hold the string or character for the day whatever be the content of this variable will be checked against the value of the RTC each time.
+  DateTime now = rtc.now();
+  // We only need the day, month and year from the RTC for our problem. Hence i will leave behind the time section
+  year_h = now.year();
+  month_h = now.month();
+  day_h = now.day();
+  holder_h = year_h.concat(month_h.concat(day_h));
+  
+  // The variable holder will then be stored in the External EEPROM.
+  // I will always compare the content of holder_i  to what the RTC gives at a particular instance that is holder_h.
+  }
+
+  
    void initial_eeprom_clearing(){
     // Function for initially clearing the EEPROM memory
     for(int i = 0 ; i < EEPROM.length() ; i++){
